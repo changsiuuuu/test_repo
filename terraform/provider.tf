039bc1fc3ba7a30e8627fb2672e36a537d73ac34
changsiuuuu <changsiuuuu@gmail.com>
2026-06-08 15:06:46 +0900

    test14

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,6 +9,16 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+
+  # terraform 상태관리 (CI/CD)
+  backend "s3" {
+    bucket         = "tfstate-bucket-0917b0f7"
+    key            = "deployment/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "terraform-lock" # 미리 준비된 dynamodb 테이블
+    encrypt        = true
+  }
 }
 
 provider "aws" {
