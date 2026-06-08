3c53867152ab30c6f0593417f8511a1a1ce6dcec
changsiuuuu <changsiuuuu@gmail.com>
2026-06-08 14:16:34 +0900

    test13

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,28 +5,8 @@ variable "region" {
 
 variable "web_ami_id" {
   type        = string
-  default     = "ami-0aef7d1237f8a3805"   # Amazon Linux 2 (서울 리전 기본값)
+  default     = "ami-0aef7d1237f8a3805"
   description = "ami-build.yml이 갱신할 AMI ID"
-}
-
-variable "instance_type" {
-  type    = string
-  default = "t3.micro"   # 프리티어
-}
-
-variable "key_name" {
-  type        = string
-  default     = "test_repo"        # 본인 키페어 이름 (SSH용)
-  description = "test_repo"
-}
-
-variable "region" {
-  type    = string
-  default = "ap-northeast-2"
-}
-
-variable "web_ami_id" {
-  type = string
 }
 
 variable "instance_type" {
@@ -35,8 +15,9 @@ variable "instance_type" {
 }
 
 variable "key_name" {
-  type    = string
-  default = ""
+  type        = string
+  default     = "test_repo"
+  description = "SSH 키페어 이름"
 }
 
 # ✨ Scaling 관련 변수
